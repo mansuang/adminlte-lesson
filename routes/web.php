79edeafd402fa927bb40504f45be9291945c7cde
Mansaung P <mansaung@hotmail.com>
2017-06-29 13:25:52 +0700

    fix sidebar auto open

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', function () { return view('index'); });
+Route::get('/index2', function () { return view('index2'); });
+Route::get('/pages/layout/top-nav', function () { return view('pages.layout.top-nav'); });
